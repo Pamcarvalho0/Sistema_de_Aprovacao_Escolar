40db1b85ceb73c7c7ffa053b5a52795236a5e1ba
Pamcarvalho0 <pamela.tavares@edu.senai.br>
2026-06-11 11:44:58 -0300

    Atividade 1

---
+++ Atividades.js
@@ -0,0 +1,13 @@
+// Atividade 1
+
+let alunos = ["Paloma", "Duda", "Gaby", "Manu", "Leticia"];
+console.log([alunos[0], alunos[4]]);
+
+// Atividade 2
+let frutas = ["Maçã", "Uva", "Laranja", "Abacaxi"];
+console.log([frutas[0]]);
+console.log([frutas[1]]);
+console.log([frutas[2]]);
+console.log([frutas[3]]);
+
+// Atividade 3
