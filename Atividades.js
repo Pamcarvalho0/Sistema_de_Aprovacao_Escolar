2765a56f9ee4a807ee810c9014fab9bff51bd7ff
Pamcarvalho0 <pamela.tavares@edu.senai.br>
2026-06-11 11:54:03 -0300

    Atividade 1

--- Atividades.js
+++ Atividades.js
@@ -1,9 +1,10 @@
 // Atividade 1
-
+console.log('--------- Atividade 1 ---------');
 let alunos = ["Paloma", "Duda", "Gaby", "Manu", "Leticia"];
 console.log([alunos[0], alunos[4]]);
 
 // Atividade 2
+console.log('--------- Atividade 2 ---------');
 let frutas = ["Maçã", "Uva", "Laranja", "Abacaxi"];
 console.log([frutas[0]]);
 console.log([frutas[1]]);
@@ -11,3 +12,7 @@ console.log([frutas[2]]);
 console.log([frutas[3]]);
 
 // Atividade 3
+console.log('--------- Atividade 3 ---------');
+let cores = ['Vermelho', 'Laranja', 'Amarelo', 'Verde', 'Azul'];
+console.log([cores[0], cores[2], cores[4]]);
+
